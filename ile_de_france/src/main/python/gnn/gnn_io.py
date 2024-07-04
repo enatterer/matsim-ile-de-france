0211f53fe62d44e94078f64c75082d4b4fc453e3
enatterer <elena.natterer@tum.de>
2024-07-04 08:53:45 +0200

    some clean ups and refactorings

--- ile_de_france/src/main/python/gnn/gnn_io.py
+++ ile_de_france/src/main/python/gnn/gnn_io.py
@@ -21,41 +21,174 @@ from shapely.geometry import LineString
 
 import pickle
 
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-def check_and_replace_inf(policy_data):
-    has_inf = False
-    inf_sources = {"capacity": 0, "freespeed": 0, "mode": 0}
+import math
+import random
+import pickle
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import tqdm
+import wandb
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as T
+from torch.utils.data import DataLoader, Dataset, Subset
+
+import torch_geometric
+from torch_geometric.data import Data, Batch
+from torch_geometric.transforms import LineGraph
+
+from shapely.geometry import LineString
+
+def validate_model(model, valid_dl, loss_func, device):
+    model.eval()
+    val_loss = 0
+    num_batches = 0
+    with torch.inference_mode():
+        for idx, data in enumerate(valid_dl):
+            input_node_features, targets = data.normalized_x.to(device), data.normalized_y.to(device)
+            predicted = model(data)
+            val_loss += loss_func(predicted, targets).item()
+            num_batches += 1
+    return val_loss / num_batches if num_batches > 0 else 0
+
+def create_dataloader(is_train, batch_size, dataset, train_ratio):
+    dataset_length = len(dataset)
+    print(f"Total dataset length: {dataset_length}")
+
+    # Calculate split index for training and validation
+    split_idx = int(dataset_length * train_ratio)
     
-    for i, row in enumerate(policy_data):
-        capacity, freespeed, modes = row[0], row[1], row[2]
+    # Calculate the maximum number of samples that fit into complete batches for training and validation
+    train_samples = (split_idx // batch_size) * batch_size
+    valid_samples = ((dataset_length - split_idx) // batch_size) * batch_size
+    if is_train:
+        indices = range(0, train_samples)
+    else:
+        indices = range(split_idx, split_idx + valid_samples)
+    sub_dataset = Subset(dataset, indices)
+    print(f"{'Training' if is_train else 'Validation'} subset length: {len(sub_dataset)}")
+    return DataLoader(dataset=sub_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
+
+class MyGeometricDataset(Dataset):
+    def __init__(self, data_list):
+        self.data_list = data_list
+    def __len__(self):
+        return len(self.data_list)
+    def __getitem__(self, idx):
+        return self.data_list[idx]
+
+class EarlyStopping:
+    def __init__(self, patience=5, verbose=False):
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+        elif val_loss > self.best_loss:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_loss = val_loss
+            self.counter = 0
+
+    
+def collate_fn(data_list):
+    return Batch.from_data_list(data_list)
+
+def normalize_data(dataset):
+    # Collect all node features
+    all_node_features = []
+    for data in dataset:
+        all_node_features.append(data.x)
+
+    # Stack all node features into a single tensor
+    all_node_features = torch.cat(all_node_features, dim=0)
+
+    # Fit the min-max scaler on the node features
+    scaler = MinMaxScaler()
+    scaler.fit(all_node_features)
+
+    # Apply the scaler to each data instance and store as a new feature
+    for data in dataset:
+        data.normalized_x = torch.tensor(scaler.transform(data.x), dtype=torch.float)
+
+    return dataset
+
+def normalize_positional_features(dataset):
+    # Collect all positional features
+    all_pos_features = []
+    for data in dataset:
+        all_pos_features.append(data.pos)
+
+    # Stack all positional features into a single tensor
+    all_pos_features = torch.cat(all_pos_features, dim=0)
+
+    # Fit the min-max scaler on the positional features
+    scaler = MinMaxScaler()
+    scaler.fit(all_pos_features)
+
+    # Apply the scaler to each data instance and store as a new feature
+    for data in dataset:
+        data.normalized_pos = torch.tensor(scaler.transform(data.pos), dtype=torch.float)
+    return dataset
+
+def normalize_y_values(dataset):
+    # Collect all y values
+    all_y_values = []
+    for data in dataset:
+        all_y_values.append(data.y)
+
+    # Stack all y values into a single tensor
+    all_y_values = torch.cat(all_y_values, dim=0)
+
+    # Fit the min-max scaler on the y values
+    scaler = MinMaxScaler()
+    scaler.fit(all_y_values)
+
+    # Apply the scaler to each data instance and store as a new feature
+    for data in dataset:
+        data.normalized_y = torch.tensor(scaler.transform(data.y), dtype=torch.float)  # Keep the 2D shape
+
+    return dataset
+
+def normalize_dataset(dataset):
+    # Normalize node features
+    dataset = normalize_data(dataset)
+    # Normalize positional features (if any)
+    dataset = normalize_positional_features(dataset)
+    # Normalize y values
+    dataset = normalize_y_values(dataset)
+    return dataset
+
+
+# def check_and_replace_inf(policy_data):
+#     has_inf = False
+#     inf_sources = {"capacity": 0, "freespeed": 0, "mode": 0}
+    
+#     for i, row in enumerate(policy_data):
+#         capacity, freespeed, modes = row[0], row[1], row[2]
         
-        # Check freespeed for inf values
-        if freespeed == float('inf') or freespeed == float('-inf'):
-            # print(f"Inf value found in freespeed at row {i}: {freespeed}")
-            has_inf = True
-            inf_sources["freespeed"] += 1
-            row[1] = 1e6 if freespeed == float('inf') else -1e6
-    return policy_data, has_inf, inf_sources
-
-def init_weights(m):
-    if isinstance(m, nn.Linear):
-        nn.init.kaiming_uniform_(m.weight, nonlinearity='relu')
-        if m.bias is not None:
-            nn.init.zeros_(m.bias)
-            
-def get_dataloader(is_train, batch_size, datasets):
-    loader = torch.utils.data.DataLoader(dataset=datasets, 
-                                         batch_size=batch_size, 
-                                         shuffle=True if is_train else False, 
-                                         pin_memory=True, num_workers=2)
-    return loader
-
-def min_max_normalize(tensor):
-    min_val = tensor.min()
-    max_val = tensor.max()
-    normalized_tensor = (tensor - min_val) / (max_val - min_val)
-    return normalized_tensor
-
+#         # Check freespeed for inf values
+#         if freespeed == float('inf') or freespeed == float('-inf'):
+#             # print(f"Inf value found in freespeed at row {i}: {freespeed}")
+#             has_inf = True
+#             inf_sources["freespeed"] += 1
+#             row[1] = 1e6 if freespeed == float('inf') else -1e6
+#     return policy_data, has_inf, inf_sources
 
 # Define a dictionary to map each mode to an integer
 mode_mapping = {
@@ -85,60 +218,10 @@ mode_mapping = {
 def encode_modes(modes):
     return mode_mapping.get(modes, -1)  # Use -1 for any unknown modes
 
-def create_edge_index_and_tensors(gdf):
-    # Ensure the GeoDataFrame is projected in a geographic CRS
-    gdf = gdf.to_crs(epsg=4326)
-    
-    # Extract unique nodes (start and end points of LineStrings)
-    nodes = {}
-    edges = []
-    car_volume_counts = []
-    capacities = []
-    freespeeds = []
-    modes_list = []
-    node_counter = 0
 
-    for idx, row in gdf.iterrows():
-        line = row['geometry']
-        car_volume = row['vol_car']  # Extract car volume
-        capacity = row['capacity']  # Extract capacity
-        freespeed = row['freespeed']  # Extract freeflow speed
-        modes = encode_modes(row['modes']) # Extract modes
-        if isinstance(line, LineString):
-            start_point = line.coords[0]
-            end_point = line.coords[-1]
-            
-            if start_point not in nodes:
-                nodes[start_point] = node_counter
-                node_counter += 1
-            if end_point not in nodes:
-                nodes[end_point] = node_counter
-                node_counter += 1
-            
-            start_index = nodes[start_point]
-            end_index = nodes[end_point]
-            
-            edges.append((start_index, end_index))
-            car_volume_counts.append(car_volume)  # Store activity count for this edge
-            capacities.append(capacity)  # Store capacity for this edge
-            freespeeds.append(freespeed)  # Store freespeed speed for this edge
-            modes_list.append(modes)  # Store modes for this edge
-    
-    # Create edge_index tensor
-    edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous()
-    
-    # Create car volume tensor
-    car_volume_tensor = torch.tensor(car_volume_counts, dtype=torch.float)
-    
-    # Create policy_tensor
-    policy_data = [[cap, fs, mode] for cap, fs, mode in zip(capacities, freespeeds, modes_list)]
-    policy_data, has_inf, inf_sources = check_and_replace_inf(policy_data)
-    # if has_inf:
-    #     print("Inf values were found and replaced in policy_data.")
-    #     print("Inf value sources:", inf_sources)
-    
-    policy_tensor = torch.tensor(policy_data, dtype=torch.float)
-    return edge_index, car_volume_tensor, policy_tensor, nodes
+
+
+
 
 # def visualize_data(policy_features, flow_features, title):
 #     edges = edge_index.T.tolist()
@@ -193,9 +276,3 @@ def create_edge_index_and_tensors(gdf):
 #     #     plt.savefig(f"visualisation/train_data/{title}.png", dpi = 500)
 #     # else:
 #     #     plt.savefig(f"visualisation/test_data/{title}.png", dpi = 500)
-    
-def create_edge_adjacency_matrix(edge_index, num_edges):
-    indices = torch.cat([edge_index, edge_index.flip(0)], dim=1)  # Make it undirected
-    values = torch.ones(indices.shape[1])
-    adj_matrix = torch.sparse.FloatTensor(indices, values, torch.Size([num_edges, num_edges]))
-    return adj_matrix
