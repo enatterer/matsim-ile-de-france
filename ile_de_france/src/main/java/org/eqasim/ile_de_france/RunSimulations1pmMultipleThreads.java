703075b13805d5b6b8b8e6efbaf1bc9b59c3b232
enatterer <elena.natterer@tum.de>
2024-09-19 18:05:22 +0200

    adapted configs

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulations1pmMultipleThreads.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulations1pmMultipleThreads.java
@@ -176,7 +176,7 @@ public class RunSimulations1pmMultipleThreads extends SimulationRunnerBase{
         String fullConfigPath = Paths.get(workingDirectory, configPath).toString();
         LOGGER.info("Running simulation with config: " + fullConfigPath + ", network file: " + networkFile + ", output directory: " + outputDirectory);
 
-        final List<String> arguments = Arrays.asList("java", "-Xms10g", "-Xmx10g","-cp",
+        final List<String> arguments = Arrays.asList("java", "-Xms16g", "-Xmx16g","-XX:+UseG1GC", "-XX:MaxGCPauseMillis=200", "-cp",
                 "ile_de_france/target/ile_de_france-1.5.0.jar",
                 "org.eqasim.ile_de_france.RunSimulation1pm",
                 "--config:global.numberOfThreads", "4",
