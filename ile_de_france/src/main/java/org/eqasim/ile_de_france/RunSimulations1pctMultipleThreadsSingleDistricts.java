7a524c3e37fac1338715457e412147e748ad01dd
enatterer <elena.natterer@tum.de>
2024-10-18 12:31:53 +0200

    adapted config

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulations1pctMultipleThreadsSingleDistricts.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulations1pctMultipleThreadsSingleDistricts.java
@@ -27,14 +27,14 @@ public class RunSimulations1pctMultipleThreadsSingleDistricts extends Simulation
     static public void main(String[] args) throws Exception {
         // Configuration settings
         String configPath = "paris_1pct_config.xml";
-        String workingDirectory = "ile_de_france/data/pop_1pct_simulations/pop_1pct_cap_reduction/";
-        String networkDirectory = "ile_de_france/data/pop_1pct_simulations/pop_1pct_cap_reduction/networks/";
+        String workingDirectory = "ile_de_france/data/pop_1pct_simulations/pop_1pct_cap_reduction/exp_dist_not_connected_5k/";
+        String networkDirectory = "ile_de_france/data/pop_1pct_simulations/pop_1pct_cap_reduction/exp_dist_not_connected_5k/networks/";
 
         // List all files in the directory
         Map<String, List<String>> networkFilesMap = getNetworkFiles(networkDirectory);
 
         // Create a fixed thread pool with 5 threads
-        ExecutorService executor = Executors.newFixedThreadPool(3);
+        ExecutorService executor = Executors.newFixedThreadPool(4);
 
         for (int i = 1000; i <= 16000; i += 1000) {
             String folder = "networks_" + i;
@@ -177,7 +177,7 @@ public class RunSimulations1pctMultipleThreadsSingleDistricts extends Simulation
         // Full path to the configuration file
         String fullConfigPath = Paths.get(workingDirectory, configPath).toString();
 
-        final List<String> arguments = Arrays.asList("java", "-Xms100g", "-Xmx100g", "-cp",
+        final List<String> arguments = Arrays.asList("java", "-Xms40g", "-Xmx40g", "-cp",
                 "ile_de_france/target/ile_de_france-1.5.0.jar",
                 "org.eqasim.ile_de_france.RunSimulation1pct",
                 "--config:global.numberOfThreads", "12",
