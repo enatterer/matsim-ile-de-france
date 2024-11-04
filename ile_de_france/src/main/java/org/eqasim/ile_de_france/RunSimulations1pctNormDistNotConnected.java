d704e77291747bf18386bab08df784ce0e8147dc
enatterer <elena.natterer@tum.de>
2024-11-04 14:54:06 +0100

    changed sbatch

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulations1pctNormDistNotConnected.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulations1pctNormDistNotConnected.java
@@ -173,8 +173,8 @@ public class RunSimulations1pctNormDistNotConnected extends SimulationRunnerBase
         // Full path to the configuration file
         String fullConfigPath = Paths.get(workingDirectory, configPath).toString();
         final List<String> arguments = Arrays.asList(
-            // "java", "-Xms64g", "-Xmx64g", 
-            "-cp",
+               "java", "-cp",
+                // "-Xms64g", "-Xmx64g", 
                 "ile_de_france/target/ile_de_france-1.5.0.jar",
                 "org.eqasim.ile_de_france.RunSimulation1pct",
                 "--config:global.numberOfThreads", "12",
